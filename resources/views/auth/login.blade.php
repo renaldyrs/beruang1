f1102052a79f4b35437fc6213e218715bf06bdb0
Shiruishi <tioferry.27@gmail.com>
2021-02-19 10:25:21 +0700

    selesai template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                   <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Login">
                 </form>
                 <a href="{{ route('password.request') }}" class="forgot-password-link">Forgot password?</a>
-                <p class="login-card-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
+                <p class="login-card-footer-text">Don't have an account? <a href="{{ route('register') }}" class="text-reset"><b>Register here</b></a></p>
                 <nav class="login-card-footer-nav">
                   <a href="#!">Terms of use.</a>
                   <a href="#!">Privacy policy</a>
