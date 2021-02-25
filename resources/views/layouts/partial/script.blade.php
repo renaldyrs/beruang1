f04b54031f166d8b5078fa117fb61ca62156f902
Shiruishi <tioferry.27@gmail.com>
2021-02-25 11:37:26 +0700

    update, navbar, menambah halaman persiapan desain untuk keranjang, profil, produk

--- resources/views/layouts/partial/script.blade.php
+++ resources/views/layouts/partial/script.blade.php
@@ -1,6 +1,8 @@
 <script src="{{asset('New/assets/js/jquery.min.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 
+
+
     <script src="{{asset('New/assets/bootstrap/js/bootstrap.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
     <script src="{{asset('New/assets/js/smoothproducts.min.js')}}"></script>
