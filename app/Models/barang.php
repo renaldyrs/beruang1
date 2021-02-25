c80d8782db5a5edfd5b70aad26bcf3ea311def3c
Ketut <adisuprianta13@gmail.com>
2021-02-25 11:21:22 +0700

    selesai admin barang

--- app/Models/barang.php
+++ app/Models/barang.php
@@ -10,5 +10,8 @@ class barang extends Model
     use HasFactory;
     protected $table = "barangs";
  
-    protected $fillable = ['file','nama','harga','stock','keterangan'];
+    protected $fillable = ['file','nama','id_category','harga','stock','keterangan'];
+    public function role(){
+        return $this->belongsTo('App\Models\category','id');
+    }
 }
