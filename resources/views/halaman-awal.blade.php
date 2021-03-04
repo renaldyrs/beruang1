de102f74d13f6cda58bbdf3d3f640c1f28e6ba87
renaldyrs <49150611+renaldyrs@users.noreply.github.com>
2021-03-04 10:35:40 +0700

    update cuy

--- resources/views/halaman-awal.blade.php
+++ resources/views/halaman-awal.blade.php
@@ -89,8 +89,7 @@ body {
                       <p class="card-text"><small class="text-muted float-left">Stock Tersedia {{$g->stock}}</small></p>
                       <br>
                    <div class="card-footer">
-                      <button type="button"  class="btn btn-outline-primary float-right"><a href="/produk">Cek</a></button>
-                      <button type="button"  class="btn btn-outline-primary float-right"><a href="halaman-beli">Beli</a></button>
+                      <button type="button"  class="btn btn-outline-primary float-right"><a href="/produk/{{$g->id}}">Cek</a></button>
                    </div>
                       
                     </div>
